9547eec4858a3593c3903f42cfb477039d0f1b14
FARIHA MALIK <163237663+farihamalik098@users.noreply.github.com>
2024-04-08 11:24:33 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,52 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+const currency : any = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 278,
+    TAKA: 109.81,
+    RIYAL: 3.75,
+    RAND: 18.64,
+    WON: 1355.14,
+};
+let user_answer = await inquirer.prompt(
+    [ 
+        {
+            name: "from",
+            message: "Enter From Currency",
+            type: "list",
+            choices: ['USD','EUR','GBP','IND','PKR','TAKA','RIYAL','RAND','WON']
+
+        },
+
+        {    name: "to",
+             message: "Enter To Currency",
+             type: "list",
+             choices: ['USD','EUR','GBP','IND','PKR','TAKA','RIYAL','RAND','WON'] 
+
+        },
+
+        {
+            name: "amount",
+            message: "Enter your Amount",
+            type: "number",
+        }
+
+    ]
+)
+
+let fromAmount = currency[user_answer.from]
+let toAmount = currency[user_answer.to]
+let amount = user_answer.amount
+let baseAmount = amount / fromAmount
+let convertedAmount = baseAmount * toAmount
+console.log(convertedAmount);
+    
+
+
+
+
